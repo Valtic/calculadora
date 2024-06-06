d46e9fb1627bf6597377b0d8377598b4149a17e5
Valtic <jose_ontur@hotmail.com>
2024-06-06 22:47:57 +0000

    App Funcional

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,106 @@
-import altair as alt
+from datetime import timedelta
 import numpy as np
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+from sqlalchemy.sql import text
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Title of the app
+title_template="""
+    <div style="background-color:rgb(235, 238, 245);border-radius:12px; padding:8px;">
+    <h1 style="color:rgb(3, 24, 66)"> Cambio de unidades volumen</h1>
+    </div> """
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# st.title('title_template')
+st.markdown(title_template, unsafe_allow_html=True)
+    
+subheader_template="""
+    <div style="background-color: white; padding:8px;">
+    <h3 style="color:rgb(202, 240, 248)"> By Valtic</h3>
+    </div> """
+    
+st.markdown(subheader_template, unsafe_allow_html=True)
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+opciones_menu=["Home", "Litros -> galones (us)",
+                     "Galones -> litros",
+                      "gpm (us) -> litros/hora",
+                       "litros/h -> gpm(us)"]
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Sidebar menu for navigation
+menu = st.sidebar.selectbox("Selecciona una opción", opciones_menu)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Home
+if menu == opciones_menu[0]: #HOME
+    st.header("Conversion unidades de Volumen")
+    st.write("Usa el menu lateral para seleccionar una calculadora")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    conn = st.connection('pets_db', type='sql')
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    with conn.session as s:
+        st.markdown(f"Note that `s` is a `{type(s)}`")
+        s.execute(text('CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT);'))
+        s.execute(text('DELETE FROM pet_owners;'))
+        pet_owners = {'jerry': 'fish', 'barbara': 'cat', 'alex': 'puppy'}
+        for k in pet_owners:
+            s.execute(text(
+                'INSERT INTO pet_owners (person, pet) VALUES (:owner, :pet);'),
+                params=dict(owner=k, pet=pet_owners[k])
+            )
+        s.commit()
+
+    pet_owners = conn.query('select * from pet_owners', ttl=timedelta(minutes=10))
+    st.dataframe(pet_owners)
+
+# gal to l.
+elif menu == opciones_menu[1] or menu == opciones_menu[2]: #"Galones -> litros"
+    st.header("Galones (us) a Litros")
+    galones = st.number_input("Escribe volumen en galones para calcular en litros",step=1.0)
+    if galones:
+        litros = galones * 3.785412
+        st.write(f"Resultado: {galones:.2f} gal son {litros:.2f} litros.")
+
+    # L to gal.
+    st.header("Litros a Galones(us)")
+    litros_2 = st.number_input("Escribe volumen en litros para calcular en galones(us)",step=1.0)
+    if litros_2:
+        galones_2 = litros_2 / 3.785412
+        st.write(f"Resultado: {litros_2:.2f} litros son {galones_2:.2f} galones.")
+
+
+# gmp to l/h.
+elif menu == opciones_menu[3] or menu == opciones_menu[4]: 
+    #"Galones por minuto -> litros/hora"
+    st.header("Galones por minuto (us) a Litros/hora")
+    gpm = st.number_input("Escribe volumen en galones para calcular en litros",step=1.0)
+    if gpm:
+        litros_hora = ( gpm * 3.785412 ) * 60
+        st.write(f"Resultado: {gpm:.2f} gpm (us) son {litros_hora:.2f} litros/hora.")
+
+# l/h to gpm.
+#elif menu == opciones_menu[4]: #"Litros/hora -> Galones por minuto"
+    st.header("Litros/hora a Galones por minuto (us)")
+    litros_hora_ = st.number_input("Escribe volumen en litros para calcular en galones(us)",step=1.0)
+    if litros_hora_:
+        galones_pmim = ( litros_hora_ / 3.785412) / 60
+        st.write(f"Resultado: {litros_hora_:.2f} litros/hora son {galones_pmim:.2f} gpm (us).")
+
+
+
+# Plot Sine Wave
+elif menu == "Plot Sine Wave":
+    st.header("Plot Sine Wave")
+    x = np.linspace(0, 10, 100)
+    y = np.sin(x)
+    plt.plot(x, y)
+    st.pyplot(plt)
+
+# Plot CSV Data
+elif menu == "Plot CSV Data":
+    st.header("Plot CSV Data")
+    uploaded_file = st.file_uploader("Choose a CSV file")
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.write(data)
+        st.line_chart(data)
+
